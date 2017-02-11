d6780a6d9570cd56033680210334b90dfed8b5de
JasonMakay <jasonmakay@jasonmakay.com>
2017-02-11 18:48:30 -0500

    rename mod, /foo prints name
    - apparently mod names are restricted to lower case alphanums and
    underscores. change the mod name (also changed the repo name).
    - print name of player with /foo.
    
    note to self: don't submit before testing

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-minetest.register_node("MineTestModTutorial:decowood", {
+minetest.register_node("mine_test_mod_tutorial:decowood", {
 	tiles = {"tutorial_decowood.png"},
 	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
 })
@@ -8,6 +8,6 @@ minetest.register_chatcommand("foo", {
 		interact = true
 	},
 	func = function(name, param)
-		return true, "You said " .. param .. "!"
+		return true, "You(" .. name .. ") said " .. param .. "!"
 	end
 })
