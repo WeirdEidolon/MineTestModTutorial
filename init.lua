355c055a5a2c120f22bcbff12ffed0506905c095
JasonMakay <jasonmakay@jasonmakay.com>
2017-02-11 16:24:30 -0500

    initial submit of modding tutorial
    - creates the MineTestModTutorial:decowood block
    - implements /foo x chat command to echo back parameter

---
+++ init.lua
@@ -0,0 +1,13 @@
+minetest.register_node("tutorial:decowood", {
+	tiles = {"tutorial_decowood.png"},
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
+})
+
+minetest.register_chatcommand("foo", {
+	privs = {
+		interact = true
+	},
+	func = function(name, param)
+		return true, "You said " .. param .. "!"
+	end
+})
