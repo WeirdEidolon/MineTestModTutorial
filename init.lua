eda768a8755f0aa2964365adddec717d05e1d56f
JasonMakay <jasonmakay@jasonmakay.com>
2017-02-11 16:35:55 -0500

    fix decowood node

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-minetest.register_node("tutorial:decowood", {
+minetest.register_node("MineTestModTutorial:decowood", {
 	tiles = {"tutorial_decowood.png"},
 	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
 })
