c4cbb9205692b59fa91cd1f239e1d9f6d32f7093
JasonMakay <jasonmakay@jasonmakay.com>
2017-02-11 19:49:08 -0500

    add /whereami to print player location

--- init.lua
+++ init.lua
@@ -4,10 +4,17 @@ minetest.register_node("mine_test_mod_tutorial:decowood", {
 })
 
 minetest.register_chatcommand("foo", {
-	privs = {
-		interact = true
-	},
-	func = function(name, param)
-		return true, "You(" .. name .. ") said " .. param .. "!"
-	end
+    privs = { interact = true },
+    func = function(name, param)
+        return true, "You(" .. arg[0] .. ") said " .. param .. "!"
+    end
+})
+
+minetest.register_chatcommand("whereami", {
+    privs = { interact = true },
+    func = function(name, param)
+        local player = minetest.get_player_by_name(name)
+        local p = player:getpos()
+        return true, p["x"] .. "," .. p["y"] .. "," .. p["z"]
+    end
 })
