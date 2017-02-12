5302505b5075c4cb690eecbbcf8edfa8ccb8e97b
JasonMakay <jasonmakay@jasonmakay.com>
2017-02-11 20:50:05 -0500

    add /readtest to read test.txt

--- init.lua
+++ init.lua
@@ -47,3 +47,42 @@ minetest.register_chatcommand("dirtbox", {
         return true, "put dirt"
     end
 })
+
+
+-- http://lua-users.org/wiki/FileInputOutput
+
+-- see if the file exists
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+-- get all lines from a file, returns an empty 
+-- list/table if the file does not exist
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  lines = {}
+  for line in io.lines(file) do 
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+
+
+minetest.register_chatcommand("readtest", {
+    privs = { interact = true },
+    func = function(name, param)
+        -- tests the functions above
+        local file = minetest.get_modpath("mine_test_mod_tutorial") .. '/test.txt'
+        local lines = lines_from(file)
+
+        -- print all line numbers and their contents
+        for k,v in pairs(lines) do
+          print('line[' .. k .. ']', v)
+        end
+        
+        return true, "read test.txt"
+    end
+})
